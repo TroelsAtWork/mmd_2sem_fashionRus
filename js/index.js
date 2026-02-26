504203484afa77247c8f9225d8549e0f78bc2d17
TroelsAtWork <tras@kea.dk>
2026-02-26 11:37:44 +0100

    add categories from api

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const categoryContainer = document.querySelector("#categorylist");
+
+fetch("https://kea-alt-del.dk/t7/api/categories")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((category) => {
+      categoryContainer.innerHTML += `<a href="productlist.html">${category.category}</a>`;
+    });
+  });
