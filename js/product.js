dde808268f0534ee43bc5f4b286848a2e2d23682
TroelsAtWork <tras@kea.dk>
2026-03-02 14:47:44 +0100

    add productdetails

---
+++ js/product.js
@@ -0,0 +1,22 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+const productContainer = document.querySelector("#productContainer");
+fetch(`https://kea-alt-del.dk/t7/api/products/${id}`)
+  .then((response) => response.json())
+  .then((data) => {
+    productContainer.innerHTML = `
+            <figure>
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp" alt="Produktbillede" class="productImage">
+                <span class="saleLabel">Udsalg!</span>
+            </figure>
+            <section class="productDetails">
+                <h2 class="productName">Produktnavn</h2>
+                <div>
+                    <p class="articleType"><span class="bold">Type: </span>${data.articletype}</p>
+                    <p class="productCategory"><span class="bold">Kategori: </span>${data.category}</p>
+                    <p class="productPrice"><span class="bold">Pris:</span> 699,-</p>
+                </div>
+                <button class="buyButton">Køb nu</button>
+            </section>
+    `;
+  });
--- js/productlist.js
+++ js/productlist.js
@@ -23,7 +23,7 @@ function showProducts(productsArr) {
           <p>Now DKK <span></span>,-</p>
           <p class="color_me_red"><span></span>%</p>
         </div>
-        <a href="product.html">Read More</a>
+        <a href="product.html?id=${product.id}"}>Read More</a>
       </article>`;
   });
 }
